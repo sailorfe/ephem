6d5c54c4b5a08831e21249390c9fcf6418438a26
sailorfe <sailorfe@proton.me>
2025-08-06 17:27:40 -0400

    remove clock

--- clock/__init__.py
+++
@@ -1,3 +0,0 @@
-from .now import get_moment
-from .cli import parse_arguments
-from .display import print_asc
--- clock/now.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime, timezone
-
-def get_moment():
-    now = datetime.now(timezone.utc)
-    return now.strftime("%Y-%m-%d"), now.strftime("%H:%M")
