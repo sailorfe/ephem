a54c805f4b3616ae6295ee8d05c0f6d9cf46ae7a
sailorfe <sailorfe@proton.me>
2025-09-17 18:36:42 -0400

    test: update cast.test for H:M:S

--- tests/commands/test_cast.py
+++ tests/commands/test_cast.py
@@ -13,23 +13,24 @@ class TestCast(unittest.TestCase):
         self.assertEqual(result, ("2025-08-18", None, None))
 
     def test_parse_event_date_and_time(self):
-        result = cast.parse_event(["2025-08-18", "15:30"])
-        self.assertEqual(result, ("2025-08-18", "15:30", None))
+        result = cast.parse_event(["2025-08-18", "15:30:00"])
+        self.assertEqual(result, ("2025-08-18", "15:30:00", None))
 
     def test_parse_event_with_title(self):
-        result = cast.parse_event(["2025-08-18", "15:30", "Test", "Chart"])
-        self.assertEqual(result, ("2025-08-18", "15:30", "Test Chart"))
+        result = cast.parse_event(["2025-08-18", "15:30:00", "Test", "Chart"])
+        self.assertEqual(result, ("2025-08-18", "15:30:00", "Test Chart"))
 
     def test_get_moment_utc(self):
-        dt_local, dt_utc, approx = cast.get_moment("2025-08-18", "15:30")
+        dt_local, dt_utc, approx = cast.get_moment("2025-08-18", "15:30:00")
 
         self.assertEqual(dt_local.year, 2025)
         self.assertEqual(dt_local.month, 8)
         self.assertEqual(dt_local.day, 18)
         self.assertEqual(dt_local.hour, 15)
         self.assertEqual(dt_local.minute, 30)
+        self.assertEqual(dt_local.second, 00)
         self.assertEqual(dt_local.tzinfo, ZoneInfo("UTC"))
-        
+
         self.assertEqual(dt_utc, dt_local)  # Should be same since both UTC
         self.assertFalse(approx)
 
